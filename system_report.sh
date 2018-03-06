5e86cf7809236f226ea9fb10b3c1cf5225f4ae31
Gabor Takacs <me@gabortakacs.com>
2018-03-06 15:58:33 +0100

    Update system_report.sh (#83)

--- system_report.sh
+++ system_report.sh
@@ -63,7 +63,7 @@ ver_line="$(flow version)" ;                      echo "* flow: $ver_line"
 ver_line="$(carthage version)" ;                  echo "* carthage: $ver_line"
 ver_line="$(convert --version | head -1)" ;       echo "* imagemagick (convert): $ver_line"
 ver_line="$(ps2ascii --version)" ;                echo "* ghostscript (ps2ascii): $ver_line"
-ver_line="$(screen --version)" ;                  echo "* screen: $ver_line"
+ver_line="$(screen --version | grep Screen)" ;    echo "* screen: $ver_line"
 
 # wine was removed, not installed on new Stacks
 set +e
