545d9caf08595ec68e3ff71b28f37dc0b47e7f4c
Laszlo Meszaros <64836569+lmesz-bitrise@users.noreply.github.com>
2020-09-28 09:11:05 +0200

    [IN-1877] Install python3.8, molecule breaks on python3.5 (#320)
    * IN-1877 Install python3.8, molecule breaks on python3.5
    
    * change ansible-lint install
    
    * ignore the problem with the dash in role name
    
    Co-authored-by: fehersanyi-bitrise <sandor.feher@bitrise.io>

--- _scripts/install_molecule_dependencies.sh
+++ _scripts/install_molecule_dependencies.sh
@@ -9,15 +9,22 @@ if [[ "$os" == "Darwin" ]]; then
     molecule_version="$(molecule --version | sed -n 1p)"
     echo "Successfully installed $molecule_version"
 else
+    #TODO: ugly hack to use molecule till we not switch to virtualenv
+    #build runs in ubuntu 16.04, python 3.5 is installed there, but it is not compatible with molecule
+    add-apt-repository -y ppa:deadsnakes/ppa
+
     echo "Updating apt..."
     apt-get update &>/dev/null
 
-    echo "Install apt dependencies..."
-    apt-get install -y build-essential libssl-dev libffi-dev python-dev &>"$BITRISE_DEPLOY_DIR"/apt_install.log
+    apt-get install -y python3.8 python3.8-dev python3.8-venv
+    update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 1
+    update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 2
+    update-alternatives --set python3 /usr/bin/python3.8
+    curl https://bootstrap.pypa.io/get-pip.py | python3.8
 
-    echo "Install pip3..."
-    apt install -y python3-pip &>/dev/null
-    pip install --upgrade pip &>/dev/null
+    echo "Install apt dependencies..."
+    apt-get install -y build-essential libssl-dev libffi-dev &>"$BITRISE_DEPLOY_DIR"/apt_install.log
+
     pip_version="$(pip --version | awk '{print $1, $2}')"
     echo "Installed $pip_version"
 
