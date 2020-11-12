299005b986f4219fd62d89fea9e0f44fa5e39803
Laszlo Meszaros <64836569+lmesz-bitrise@users.noreply.github.com>
2020-11-12 09:21:05 +0100

    added more tests to cmd-bridge and added the tests to baseStackRemote (#24)

--- roles/cmd-bridge/tests/test_cmd-bridge.py
+++ roles/cmd-bridge/tests/test_cmd-bridge.py
@@ -12,3 +12,11 @@ class TestIntermediateSetup(unittest.TestCase):
         self.assertTrue(self.host.file("/Users/vagrant/Library/LaunchAgents/").exists, "LaunchAgents directory does not exists for cmd-bridge")
         self.assertTrue(self.host.file("/Users/vagrant/Library/LaunchAgents/bitrise.io.tools.cmd-bridge.plist").exists, "bitrise.io.tools.cmd-bridge.plist is not at the correct place")
         self.assertTrue(self.host.file("/Users/vagrant/logs").exists, "Log directory for cmd-bridge has not been created")
+
+    def test_cmd_bridge_is_runnins(self):
+        self.assertEqual(self.host.run("ps aux | grep cmd-bridge").rc, 0)
+
+    def test_cmd_bridge_is_using_the_correct_log_files(self):
+        cmd_bridge_log = self.host.file("/Users/vagrant/logs/bitrise.io.tools.cmd-bridge.log")
+        self.assertTrue(cmd_bridge_log.exists)
+        self.assertEqual(cmd_bridge_log.content_string.count("Ready to serve on port"), 1)
